89482fdbed66cbc9929f3ce32c43e1f97e5bf9c5
divaharsha <divakarvan03@gmail.com>
2023-02-07 12:43:43 +0530

    crud updated

--- includes/crud.php
+++ includes/crud.php
@@ -1,7 +1,7 @@
 <?php
 
 
-define('DOMAIN_URL', 'https://localhost/abcd_land/'); /* chnage to your domain here - don't forget to add forward slash at the end of the URL like this "/" */
+define('DOMAIN_URL', 'https://abcd-dashboard.greymatterworks.in/'); /* chnage to your domain here - don't forget to add forward slash at the end of the URL like this "/" */
 define('ALLOW_MODIFICATION', 1);
 define('JWT_SECRET_KEY', '12345678'); /* chnage to your secret key here set it to anything you like*/
 define('COST_PER_CODE', 0.17);
@@ -18,9 +18,9 @@ class Database
      */
 
     private $db_host = "localhost";
-    private $db_user = "root";
-    private $db_pass = ""; 
-    private $db_name = "abcd"; 
+    private $db_user = "u743445510_abcd";
+    private $db_pass = "Abcd@2022"; 
+    private $db_name = "u743445510_abcd"; 
 
 
     //LIve
